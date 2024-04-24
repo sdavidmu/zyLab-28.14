609925a9a06a167f459beb02c66abe71cd17bd1f
Ayaansheikh2504 <167333386+Ayaansheikh2504@users.noreply.github.com>
2024-04-24 11:06:43 -0500

    Update main (1).cpp

--- main (1).cpp
+++ main (1).cpp
@@ -4,18 +4,41 @@ using namespace std;
 
 #include "ShoppingCart.h"
 
-void PrintMenu() {
-   /* Type your code here */
-   
-}
-
-void ExecuteMenu(char option, ShoppingCart& theCart) {
-   /* Type your code here */
-   
-}
-
+void PrintMenu()
+void ExecuteMenu(char option, ShoppingCart& theCart);
 int main() {
-   /* Type your code here */
+   string customerName, todayDate;
+   cout<<"Enter Customer Name:"<<endl;
+   getline(cin, customerName);
+   cout<<"enter todays date"<<endl;
+   getline(cin, todayDate);
+      //output customer name and date
+      cout<<endl<<"Customer Name:" <<customerName<<endl;
+      cout<<"Todays Date:"<<todayDate<<endl;
+   //create and object type of shopping cart
+   ShoppingCart cart (customerName, todayDate);
+   char option;
+
+   do{
+      cout<<"Choose an option:"<<endl;
+      cin>>option
+      cin.ignore();
+      //execute the chosen option 
+      ExecuteMenu(option, cart);
+      cout<<endl;
+   }while (option !='q');
    
    return 0;
 }
+void PrintMenu(){
+   //print the menu
+cout<<"n/Menu"<<endl;
+cout<<"a- add item to cart"<<endl;
+cout<<"d- remove item from cart"<<endl;
+cout<<"c-change item quanity"<<endl;
+cout<<"i-output items from descriptions"<<endl;
+cout<<"o-output shopping cart"<<endl;
+cout<<"q- quit"<<endl;
+}
+
+
